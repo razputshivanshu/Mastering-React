e24fd928c559beb5beb87b146309facc9546b05a
razputshivanshu <razputshivanshu@gmail.com>
2025-04-19 16:01:27 +0530

    Starting with react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+     <h1>I am going to take control of my life again!!!!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ basicreact01/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I am going to become on of the best coder of the world!!! | Shivanshu Chauhan</h1>
+    </div>
+  );
+}
+
+export default App;
